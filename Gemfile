b250b821c979414fa298c4214f2653e149d2e02a
Will Diable <wtdiable@icloud.com>
2022-02-15 12:28:21 +0000

    foundations begun

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem 'capybara'
+gem 'rspec'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'webrick'
+gem 'pg'
---
+++ db/migrations/01_create_kewtbnb_table.sql
@@ -0,0 +1,23 @@
+psql
+
+CREATE DATABASE "kewtbnb";
+
+\c kewtbnb
+
+CREATE TABLE users(id SERIAL PRIMARY KEY);
+
+CREATE TABLE spaces(id SERIAL PRIMARY KEY);
+
+CREATE TABLE accounts(id SERIAL PRIMARY KEY);
+
+\c will (replace will with your name)
+
+CREATE DATABASE "kewtbnb_test";
+
+\c kewtbnb_test
+
+CREATE TABLE users(id SERIAL PRIMARY KEY);
+
+CREATE TABLE spaces(id SERIAL PRIMARY KEY);
+
+CREATE TABLE accounts(id SERIAL PRIMARY KEY);
