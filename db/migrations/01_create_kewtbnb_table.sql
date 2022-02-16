77383ff95e7c5a4ff4da9dbb1cd18b4139f50c15
Will Diable <wtdiable@icloud.com>
2022-02-16 14:32:03 +0000

    migrations draft 0.9

--- db/migrations/01_create_kewtbnb_table.sql
+++ db/migrations/01_create_kewtbnb_table.sql
@@ -1,22 +1,63 @@
 psql -- enter REPL
 
-CREATE DATABASE "kewtbnb"; -- Create DB called 'Kewtbnb'
+-- Create first database called 'kewtbnb'
+CREATE DATABASE "kewtbnb";
 
-\c kewtbnb -- connect to kewtbnb db
+-- connect to kewtbnb database
+\c kewtbnb 
 
-CREATE TABLE users(id SERIAL PRIMARY KEY); -- create user table with id column
 
-CREATE TABLE spaces(id SERIAL PRIMARY KEY); -- create spaces table with id column
+------------ users table: (keeps record of user data using foreign_keys to link to spaces and accounts tables)
 
-CREATE TABLE accounts(id SERIAL PRIMARY KEY); -- create accounts table with id column
+-- create users table with a primary id column. Using 'SERIAL' will increment the id column automatically
+CREATE TABLE users(id SERIAL PRIMARY KEY); 
 
-ALTER TABLE accounts ADD COLUMN "user_name" varchar(60); -- add user_name column
+-- add account_id column to users table using integer (int4) data-type (4 = maximum of 4 bits) and makes the column a foreign_key
+ALTER TABLE users ADD COLUMN "account_id" int4, ADD FOREIGN KEY ("account_id") REFERENCES accounts ("id");
 
-ALTER TABLE accounts ADD COLUMN "passwords" varchar(30); -- add passwords column
+-- add owned_id column to users table using integer (int4) data-type (4 = maximum of 4 bits) and makes the column a foreign_key
+ALTER TABLE users ADD COLUMN "owned_id" int4, ADD FOREIGN KEY ("owned_id") REFERENCES spaces ("id");
 
+-- add rented_id column to users table using integer (int4) data-type (4 = maximum of 4 bits) and makes the column a foreign_key
+ALTER TABLE users ADD COLUMN "rented_id" int4, ADD FOREIGN KEY ("rented_id") REFERENCES spaces ("id");
+
+
+------------ spaces table: (keeps record of spaces and their availability)
+
+-- create spaces table with a primary id column. Using 'SERIAL' will increment the id column automatically
+CREATE TABLE spaces(id SERIAL PRIMARY KEY); 
+
+-- add available column to spaces table using boolean (bool) data-type
+ALTER TABLE spaces ADD COLUMN "available" bool;
+
+-- add price column to spaces table using integer (in4) data-type (4 = maximum of 4 bits)
+ALTER TABLE spaces ADD COLUMN "price" int4;
+
+-- add name column to spaces table using string (varchar) data-type (60 maximum characters)
+ALTER TABLE spaces ADD COLUMN "name" varchar(60); 
+
+-- add description column to spaces table using string (varchar) data-type (320 maximum characters)
+ALTER TABLE spaces ADD COLUMN "description" varchar(320); 
+
+
+------------ accounts table: (keeps record of user account details)
+
+
+-- create accounts table with a primary id column. Using 'SERIAL' will increment the id column automatically
+CREATE TABLE accounts(id SERIAL PRIMARY KEY);
+
+-- add user_name column to accounts table using string (varchar) data-type (60 maximum characters)
+ALTER TABLE accounts ADD COLUMN "user_name" varchar(60);
+
+-- add passwords column to accounts table using string (varchar) data-type (30 maximum characters)
+ALTER TABLE accounts ADD COLUMN "passwords" varchar(30);
+
+-- add email column to accounts table using string (varchar) data-type (320 maximum characters)
 ALTER TABLE accounts ADD COLUMN "email" varchar(320); -- add email column
 
 
-\c will -- replace 'will' with your name. Connects to the db parent directory 
+\c will -- replace 'will' with your name. Connects to the database parent directory ready to create the test database
+
+------------ create kewtbnb_test database:
 
 -- * REPEAT ABOVE FOR 'kewtbnb_test' DATABASE * --
