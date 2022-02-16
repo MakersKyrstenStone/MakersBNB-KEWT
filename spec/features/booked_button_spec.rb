ae45e29bd1997557db2648174f7cae2dd803005e
Kyrsten <Kyrsten>
2022-02-16 15:42:48 +0000

    booked button spec tom edit

--- spec/features/booked_button_spec.rb
+++ spec/features/booked_button_spec.rb
@@ -3,7 +3,7 @@ feature 'booking button' do
   scenario' a user can click button to book space' do 
 
     visit('/spaces')
-    click_button "BOOK!"
+    first(:button, "BOOK!").click
     expect(page).to have_content 'Booking request received.'
   end
 end
