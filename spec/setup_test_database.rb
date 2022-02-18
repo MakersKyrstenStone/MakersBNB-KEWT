24d656c83d336752327ad7ea4dde6b98df241d29
Will Diable <wtdiable@icloud.com>
2022-02-18 12:04:48 +0000

    populating

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -14,11 +14,8 @@ def populate_test_database
 
   connection.exec("INSERT INTO accounts (user_name,passwords) 
   VALUES('Will','pw1'), ('Eden','pw2'), ('Tom','pw3'), ('Kyrsten','pw4');")
-
-  connection.exec("INSERT INTO accounts (user_name,passwords) 
-  VALUES('Will','pw1'), ('Eden','pw2'), ('Tom','pw3'), ('Kyrsten','pw4');")
   
-  connection.exec("INSERT INTO spaces (price,name) 
-  VALUES(100,'Standard flat'), (5000,'Desert Island'), (2500,'Palace'), (600,'Beautiful Country Estate');")
+  connection.exec("INSERT INTO spaces (available,price,name,description) 
+  VALUES(TRUE,100,'Standard flat','Not many rats'), (TRUE,5000,'Desert Island','An island to yourself!'), (TRUE,2500,'Palace','Roomy'), (TRUE,600,'Beautiful Country Estate','I am Beautiful!');")
   
 end
